49f88266ff4a03c4d0a20d2c057bc997f834c5bc
rajkonkret <rajkonkret660@gmail.com>
2020-06-11 23:46:15 +0200

    some change in keyboard interrupt

--- snake.py
+++ snake.py
@@ -5,6 +5,7 @@ white = (255,255,255)
 red = (255,0,0)
 black = (0,0,0)
 map_size = 900
+x = 0
 pygame.init()
 screen = pygame.display.set_mode((map_size, map_size*3//4))
 pygame.display.set_caption("Snake")
@@ -13,6 +14,8 @@ main_Font = pygame.font.SysFont('Calibri', 170)
 banner_Font = pygame.font.SysFont('Calibri', 100)
 pygame.font.init()
 screen.fill((141,141,141))
+print(pygame.key.get_repeat())
+pygame.key.set_repeat(1,1)
 
 def end_game():
     pygame.quit()
@@ -21,8 +24,8 @@ def end_game():
 def banner():
     screen.blit(banner_Font.render("Score: ", False, white),(0,0))
 
-def show_snake():
-    screen.blit(banner_Font.render("O", False, red),(400,400))
+def show_snake(x,y):
+    screen.blit(banner_Font.render("O", False, red),(x,y))
 
 while True:
   
@@ -31,6 +34,15 @@ while True:
             
             print("break")
             end_game()
+        if action.type is pygame.KEYDOWN:
+            if action.key is pygame.K_q:
+                x=x-1
+                print(x)
+        #keys = pygame.key.get_pressed()
+       
+                 
+
         banner()
-        show_snake()
+        screen.fill((141,141,141))
+        show_snake(400+x,400)
     pygame.display.update()
