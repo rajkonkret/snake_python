16c682b0c723e8a75c8c9143187b645ee345c47b
rajkonkret <rajkonkret660@gmail.com>
2020-06-11 22:35:46 +0200

    initialize screen

--- snake.py
+++ snake.py
@@ -3,16 +3,20 @@ import sys
 import random
 white = (255,255,255)
 black = (0,0,0)
-map_size = 500
-
+map_size = 900
 pygame.init()
-screen = pygame.display.set_mode((map_size, map_size))
-pygame.display.set_caption("Kółko i Krzyżyk")
+screen = pygame.display.set_mode((map_size, map_size*3//4))
+pygame.display.set_caption("Snake")
 #screen.blit(circle_surface, 0)
 main_Font = pygame.font.SysFont('Calibri', 170)
 banner_Font = pygame.font.SysFont('Calibri', 100)
+pygame.font.init()
+screen.fill((141,141,141))
 
-
+def end_game():
+    pygame.quit()
+    sys.exit()
+    
 while True:
   
     for action in pygame.event.get():
