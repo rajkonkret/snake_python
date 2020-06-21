b373eb8342b03a0fd146fb1cf87081d9d1c2c918
rajkonkret <rajkonkret660@gmail.com>
2020-06-21 23:09:05 +0200

    add add_apple()

--- snake.py
+++ snake.py
@@ -10,22 +10,31 @@ black = (0,0,0)
 map_size = 900
 x,y = 0,0
 dx, dy =0,0
+
 pygame.init()
 screen = pygame.display.set_mode((map_size, map_size*3//4))
 pygame.display.set_caption("Snake")
 #screen.blit(circle_surface, 0)
+
 snake_font_size =30
 main_Font = pygame.font.SysFont('Calibri', 170)
 banner_Font = pygame.font.SysFont('Calibri', 70)
 snake_Font = pygame.font.SysFont('Calibri', snake_font_size)
 pygame.font.init()
+
 screen.fill((141,141,141))
 print(pygame.key.get_repeat())
 pygame.key.set_repeat(1,1)
 snake_all = []
+apple_all = []
 class Snake_element:
     def __init__(self,number,x,y):
         self.number = number
+        self.x = x
+        self.y = y
+
+class Apple_element:
+    def __init__(self,x,y):
         self.x = x
         self.y = y
 
@@ -88,10 +97,14 @@ def snake_growth(d_x, x, y):
         print(d_x)
         print("len snake: ", len(snake_all))
         
+def add_apple():
+    apple_x = random.randint(0,40)
+    apple_y = random.randint(0,40)
+    apple_all.append(Apple_element(apple_x, apple_y))
+
 def show_apple():
-    apple_x = random.randint(0,20)
-    apple_y = random.randint(0,20)
-    screen.blit(snake_Font.render('X', False, blue),(apple_x * 20, apple_y * 20))
+    for apple in apple_all:
+        screen.blit(snake_Font.render('X', False, blue),(apple.x * 21, apple.y * 21))
 
 
 while True:
@@ -127,8 +140,10 @@ while True:
     dx +=x
     dy +=y
     show_snake(snake_all,x,y)
-    show_apple()
+    if dx+dy % 5 == 0 and dx+dy != 0:
+        add_apple()
     #print(dx)
+    show_apple()
     snake_growth(dx,x,y)
    
     time.sleep(1/8)
